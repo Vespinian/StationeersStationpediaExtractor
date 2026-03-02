ea90355174f3401bf6a73bc0f0ac11fd17263ea5
Vespinian <vespinian@proton.me>
2026-03-02 00:38:19 -0500

    fixed newtonjson version mismatch by using game dll

--- build.sh
+++ build.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 dotnet build
-# echo Copying dll to Stationeers directory
-# cp bin/Debug/net48/IC10OCD.dll '/mnt/sofia/SteamLibrary/steamapps/compatdata/544550/pfx/drive_c/users/steamuser/Documents/My Games/Stationeers/mods/IC10OCD/'
+echo Copying dll to Stationeers directory
+cp bin/Debug/net48/StationpediaExtractor.dll '/mnt/sofia/SteamLibrary/steamapps/compatdata/544550/pfx/drive_c/users/steamuser/Documents/My Games/Stationeers/mods/StationpediaExport/'
 # echo Copying pdb to Stationeers directory
-# cp bin/Debug/net48/IC10OCD.pdb '/mnt/sofia/SteamLibrary/steamapps/compatdata/544550/pfx/drive_c/users/steamuser/Documents/My Games/Stationeers/mods/IC10OCD/'
-# echo Copying GameData to Stationeers directory
-# cp -r GameData '/mnt/sofia/SteamLibrary/steamapps/compatdata/544550/pfx/drive_c/users/steamuser/Documents/My Games/Stationeers/mods/IC10OCD/'
-# echo Copying About to Stationeers directory
-# cp -r About '/mnt/sofia/SteamLibrary/steamapps/compatdata/544550/pfx/drive_c/users/steamuser/Documents/My Games/Stationeers/mods/IC10OCD/'
+cp bin/Debug/net48/StationpediaExtractor.pdb '/mnt/sofia/SteamLibrary/steamapps/compatdata/544550/pfx/drive_c/users/steamuser/Documents/My Games/Stationeers/mods/StationpediaExport/'
